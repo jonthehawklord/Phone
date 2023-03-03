c76a271707eedf323469d5472a46410f6c4e6df5
jonthehawklord <93273654+jonthehawklord@users.noreply.github.com>
2023-03-03 08:57:11 +0000

    1

---
+++ server/index.js
@@ -0,0 +1,100 @@
+const express = require('express')
+const path = require('path')
+const morgan = require('morgan')
+const cors = require('cors')
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(morgan((tokens, req, res) => {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'), '-',
+      tokens['response-time'](req, res), 'ms',
+      JSON.stringify(req.body)
+    ].join(' ')
+  }))
+
+let persons = [
+    { 
+      "name": "Arto Hellas", 
+      "number": "040-123456",
+      "id": 1
+    },
+    { 
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523",
+      "id": 2
+    },
+    { 
+      "name": "Dan Abramov", 
+      "number": "12-43-234345",
+      "id": 3
+    },
+    { 
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122",
+      "id": 4
+    }
+  ]
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  console.log(id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+	response.status(400).send('Current password does not match');
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  console.log(id)
+  persons = persons.find(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+app.post('/api/persons', (request, response, next) => {
+    const body = request.body
+
+    const personName = body.name
+    const personNumber = body.number
+
+    if (Object.keys(body).length === 0) {
+        return response.status(400).json({
+          error: 'content missing'
+        })
+    }
+	
+	const person = {
+        name: personName,
+        number: personNumber
+        }
+
+	persons = persons.concat(person)
+	response.json(persons)
+})
+
+
+/* 
+  // All remaining requests return the React app, so it can handle routing.
+  app.get('*', function(request, response) {
+    response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
+  }); */
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
