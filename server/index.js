84aa5c1b0a63b54640804d4bb25e5aa292776b81
jonthehawklord <93273654+jonthehawklord@users.noreply.github.com>
2023-03-03 10:10:16 +0000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static('build'))
+app.use(express.static('../react-ui/build'))
 app.use(morgan((tokens, req, res) => {
     return [
       tokens.method(req, res),
